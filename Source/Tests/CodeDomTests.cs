7b8aba12a3cde906eccdfdaa7ad80fa621802bea
Josh McCullough <josh@quantumconcepts.io>
2015-08-08 21:40:33 +0000

    Added Tests project.

---
+++ Source/Tests/CodeDomTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuantumConcepts.CodeGenerator.Core.Templates;
+using QuantumConcepts.CodeGenerator.Core.ProjectSchema;
+using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.IO;
+using Microsoft.CSharp;
+
+namespace Tests {
+    [TestClass]
+    public class CodeDomTests {
+        [TestMethod]
+        public void TestDataObjects() {
+            TestHelper helper = new TestHelper(1);
+            Project project = helper.CreateProject();
+            DataObjects template = new DataObjects(project);
+            CodeCompileUnit code = template.Render(new Template(project, @"C:\Nothing.xslt", TemplateOutputMode.SingleFile, @"C:\Nothing", null));
+            CodeDomProvider provider = new CSharpCodeProvider();
+            CodeGeneratorOptions options = new CodeGeneratorOptions() {
+                VerbatimOrder = true
+            };
+
+            using (StreamWriter writer = new StreamWriter("C:\\DataObjects_Test.cs"))
+                provider.GenerateCodeFromCompileUnit(code, writer, options);
+        }
+    }
+}
---
+++ Source/Tests/TestHelper.cs
@@ -0,0 +1,72 @@
+﻿using QuantumConcepts.CodeGenerator.Core.ProjectSchema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuantumConcepts.Common.Extensions;
+using QuantumConcepts.CodeGenerator.Core.Data.SqlServer;
+
+namespace Tests {
+    public class TestHelper {
+        public Random Random { get; private set; }
+
+        public TestHelper() {
+            this.Random = new Random();
+        }
+
+        public TestHelper(int seed) {
+            this.Random = new Random(seed);
+        }
+
+        public Project CreateProject(int tableCount = 10) {
+            Project project = new Project();
+
+            ProjectContext.Initialize(project);
+
+            project.DataTypeMappings = (new SqlServerWorker()).GetDataTypeMappingConfigurations().Single(o => o.Language == "CSharp").DataTypeMappings;
+
+            for (int i = 0; i < tableCount; i++)
+                project.TableMappings.Add(CreateTableMapping(project));
+
+            return project;
+        }
+
+        public TableMapping CreateTableMapping(Project project) {
+            string random = this.Random.Next().ToString();
+            string tableName = "Table{0}".FormatString(random);
+            string className = "Class{0}".FormatString(random);
+            TableMapping table = new TableMapping() {
+                ClassName = className,
+                SchemaName = "dbo",
+                TableName = tableName
+            };
+
+            for (int i = 0; i < this.Random.Next(1, 25); i++)
+                table.ColumnMappings.Add(CreateColumnMapping(table));
+
+            table.ColumnMappings.First().PrimaryKey = true;
+            table.JoinToProject(project);
+
+            return table;
+        }
+
+        public ColumnMapping CreateColumnMapping(TableMapping table) {
+            string random = this.Random.Next().ToString();
+            string columnName = "Column{0}".FormatString(random);
+            string fieldName = "Field{0}".FormatString(random);
+            DataTypeMapping dataType = table.ContainingProject.DataTypeMappings.Random(this.Random);
+            bool nullable = (dataType.Nullable ? this.Random.NextBool() : false);
+            ColumnMapping column = new ColumnMapping() {
+                ColumnName = columnName,
+                DatabaseDataType = dataType.DatabaseDataType,
+                DataType = dataType.ApplicationDataType,
+                FieldName = fieldName,
+                Nullable = nullable,
+                NullableInDatabase = nullable
+            };
+
+            return column;
+        }
+    }
+}
