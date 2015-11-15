07837ef6c5c1dfe7fcf49ac16d3e6249a4a383bd
Josh McCullough <josh@quantumconcepts.io>
2015-11-15 11:39:58 -0500

    Fixed #1079 - Allow exclusion of entire schema.

--- Source/Client/UI/Controls/SchemaTreeNode.cs
+++ Source/Client/UI/Controls/SchemaTreeNode.cs
@@ -1,4 +1,11 @@
-﻿namespace QuantumConcepts.CodeGenerator.Client.UI.Controls
+﻿using QuantumConcepts.CodeGenerator.Core.ProjectSchema;
+using QuantumConcepts.Common.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace QuantumConcepts.CodeGenerator.Client.UI.Controls
 {
     internal class SchemaTreeNode : ProjectSchemaTreeNode
     {
@@ -9,13 +16,45 @@
         {
             this.SchemaName = schemaName;
 
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            this.ContextMenu = new ContextMenu();
+            this.ContextMenu.MenuItems.Add(new MenuItem("Exclude From Project"));
+            this.ContextMenu.MenuItems[0].Checked = GetSchemaTableMappings().All(o => o.Exclude);
+            this.ContextMenu.MenuItems[0].Click += new EventHandler(ExcludeFromProjectMenuItem_Click);
+
             UpdateNode();
             this.Expand();
+        }
+
+        private IEnumerable<TableMapping> GetSchemaTableMappings()
+        {
+            return this.ProjectNode.Project
+                .TableMappings
+                .Where(o => string.Equals(o.SchemaName, this.SchemaName, StringComparison.InvariantCultureIgnoreCase));
         }
 
         public override void UpdateNode()
         {
             this.Text = this.SchemaName;
+        }
+
+        void ExcludeFromProjectMenuItem_Click(object sender, EventArgs e)
+        {
+            MenuItem menuItem = (MenuItem)sender;
+
+            menuItem.Checked = !menuItem.Checked;
+            GetSchemaTableMappings().ForEach(o => o.Exclude = menuItem.Checked);
+            this.Nodes.Cast<TableOrViewTreeNode>().ForEach(o =>
+            {
+                if (o != null)
+                    o.UpdateNode();
+            });
+
+            UpdateNode();
         }
     }
 }
