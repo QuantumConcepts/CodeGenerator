4c1f94669238f5b56753aed85652d4a9b8a80184
Josh McCullough <josh@quantumconcepts.io>
2016-03-02 12:42:57 -0500

    Fixed #9 - Use semantic versioning.

--- Source/Client/Properties/AssemblyInfo.cs
+++ Source/Client/Properties/AssemblyInfo.cs
@@ -7,6 +7,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("CodeGenerator")]
 [assembly: AssemblyCopyright("© Quantum Concepts Corporation")]
 [assembly: AssemblyTrademark("© Quantum Concepts Corporation")]
-[assembly: AssemblyVersion("1.7.4.0")]
-[assembly: AssemblyFileVersion("1.7.4.0")]
+[assembly: AssemblyVersion("1.7.5")]
+[assembly: AssemblyFileVersion("1.7.5")]
 [assembly: ComVisibleAttribute(false)]
